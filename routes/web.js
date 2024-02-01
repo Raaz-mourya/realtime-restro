5e6f09bbb96edebafd2ae1d7bee650b8643cede3
Raj Singh <raj123singhmourya@gmail.com>
2024-02-02 00:35:13 +0530

    1st commit: login and register complete

--- routes/web.js
+++ routes/web.js
@@ -1,19 +1,20 @@
-const homeController = require('../app/http/controllers/homeController')
-const authController = require('../app/http/controllers/authController')
-const cartController = require('../app/http/controllers/customers/cartController')
-const offerController = require('../app/http/controllers/offerController')
+const homeController = require("../app/http/controllers/homeController");
+const authController = require("../app/http/controllers/authController");
+const cartController = require("../app/http/controllers/customers/cartController");
+const offerController = require("../app/http/controllers/offerController");
 
+// Middlewares
+const guest = require("../app/http/middleware/guest");
 
 function initRoutes(app) {
+  app.get("/", homeController().index);
+  app.get("/offer", offerController().offer);
+  app.get("/login", guest, authController().login);
+  app.post("/login", authController().postLogin);
+  app.get("/register", guest, authController().register);
+  app.post("/register", authController().postRegister);
 
-    app.get('/', homeController().index)
-    app.get('/offer', offerController().offer)
-    app.get('/login', authController().login)
-    app.get('/register', authController().register)
-
-    
-    app.get('/cart',cartController().index)
-    app.post('/update-cart', cartController().update)
-
+  app.get("/cart", cartController().index);
+  app.post("/update-cart", cartController().update);
 }
-module.exports = initRoutes
+module.exports = initRoutes;
--- server.js
+++ server.js
@@ -1,65 +1,79 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const ejs = require('ejs')
-const path = require('path')
-const expressLayout = require('express-ejs-layouts')
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 
-const PORT = process.env.PORT || 3000
-const mongoose = require('mongoose')
-const session = require('express-session')
-const flash = require('express-flash')
-const MongoDbStore = require('connect-mongo');
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
 
-
+mongoose.set("strictQuery", false); // to hide special warning
 //Database connection
-const url = 'mongodb://localhost/pizza';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
+const url = "mongodb://localhost:27017/pizza";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
-connection.once('open', function () {
-    console.log('Database connected...');
-}).on('error', function (err) {
-    console.log('Connection failed...'+err)
-});
+connection
+  .once("open", function () {
+    console.log("Database connected...");
+  })
+  .on("error", function (err) {
+    console.log("Connection failed..." + err);
+  });
+
 
 //session store
 let mongoStore = new MongoDbStore({
-    mongoUrl: url,
-    collection: 'sessions'
-})
+  mongoUrl: url,
+  collection: "sessions",
+});
 
 //session config
-app.use(session({
-   secret: process.env.COOKIE_SECRET,
-   resave: false,
-   store: mongoStore,
-   saveUninitialized: false,
-   cookie: { maxAge: 1000 * 60 * 60 * 24 } //24 hours
-}))
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24 hours
+  })
+);
+
+// Passport config
+
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-app.use(flash())
+
+app.use(flash());
 
 //Asset
-app.use(express.static('public'))
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Global middleware
 app.use((req, res, next) => {
-    res.locals.session = req.session 
-    next()
-})
-
+    res.locals.session = req.session;
+    res.locals.user = req.user;
+  next();
+});
 
 //set template engine
-app.use(expressLayout)
-app.set('views',path.join(__dirname, '/resources/views'))
-app.set('view engine','ejs')
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-require('./routes/web')(app)
+require("./routes/web")(app);
 
-
-app.listen(PORT , () => {
-    console.log(`Listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
